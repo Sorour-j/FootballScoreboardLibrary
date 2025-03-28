ce5f5806f5196898a1955e7e71abf8f563e78e21
Sorour <sorour.jahanbin@yahoo.com>
2025-03-28 12:39:38 +0100

    Update TeamTest

--- src/test/java/com/footballscoreboard/model/TeamTest.java
+++ src/test/java/com/footballscoreboard/model/TeamTest.java
@@ -1,5 +1,34 @@
 package test.java.com.footballscoreboard.model;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import main.java.com.footbalscoreboard.model.Team;
+
 public class TeamTest {
 
+	
+	 @Test
+	    public void testTeamCreation() {
+		//Test that a team is created correctly with the given name
+		 	Team team = new Team("Mexico");
+	        assertEquals("Mexico", team.getName());
+	    }
+
+	    @Test
+	    public void testSetAndGetName() {
+	    //Test the set and get methods
+	        Team team = new Team("Canada");
+	        assertEquals("Canada", team.getName());
+	        team.setName("Canada");
+	        assertEquals("Canada", team.getName());
+	    }
+	    
+	    @Test
+	    public void testTeamNameMustStartWithUppercase() {
+	    // Ensure team name must start with upper-case
+	        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+	            new Team("liverpool");
+	        });
+	    }
 }
