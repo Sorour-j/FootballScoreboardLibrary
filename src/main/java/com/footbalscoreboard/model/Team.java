ac944c11641cb3d9ac4cdbc887a610c905a961e8
Sorour <sorour.jahanbin@yahoo.com>
2025-03-28 12:50:31 +0100

    Update Team

--- src/main/java/com/footbalscoreboard/model/Team.java
+++ src/main/java/com/footbalscoreboard/model/Team.java
@@ -1,4 +1,16 @@
 package main.java.com.footbalscoreboard.model;
 
 public class Team {
+	
+	private final String name; // the name of the team can not be changed
+	
+	// team can be created using name
+	public Team(String name) {
+		this.name = name;
+	}
+	
+	// return the name of the team
+	public String getName() {
+		return name;
+	}
 }
